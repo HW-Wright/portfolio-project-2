bd879e03c748146a1c136c4c0dd391fc354cf269
blahblahblah589 <hwright589@icloud.com>
2023-03-02 16:51:57 +0000

    add javascript code structure

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+})
+
+const questions = [
+    
+]
+
+/**
+ * To cycle through the questions 
+ */
+function runQuiz() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function keepScore() {
+
+}
+
+function displayScore() {
+
+}
+
